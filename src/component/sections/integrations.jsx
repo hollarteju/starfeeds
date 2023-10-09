fc99aca511cff96d9b94161025f37c38a08d7a7d
hollarteju <sirteju2022@gmail.com>
2023-10-09 23:30:29 +0100

    integration table

--- src/component/sections/integrations.jsx
+++ src/component/sections/integrations.jsx
@@ -2,24 +2,22 @@ import React from "react";
 
 function Integration(){
     return(
-        <section class="intro">
-  <div class="bg-image h-100">
-    <div class="mask d-flex align-items-center h-100">
-      <div class="container">
+    <div className="container-fluid p-0 overflow-hidden">
+      <div class="container-fluid-int p-0">
         <div class="row justify-content-center">
-          <div class="col-12">
+          <div >
            
               <div class="card-body p-0">
                 <div class="table-responsive table-scroll" data-mdb-perfect-scrollbar="true" style={{position: "relative", height: "700px"}}>
                   <table class="table mb-0">
                     <thead >
                       <tr id="tr">
-                        <th scope="col" className="px-3">Name Of Application</th>
-                        <th scope="col">Company</th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Date Of Registration</th>
-                        <th scope="col">Last Visited</th>
-                        <th scope="col">Action</th>
+                        <th  className="px-3">Name Of Application</th>
+                        <th >Company</th>
+                        <th >Category</th>
+                        <th >Date Of Registration</th>
+                        <th >Last Visited</th>
+                        <th >Action</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -30,7 +28,7 @@ function Integration(){
                             </div>
                             <span> Like a butterfly</span>
                         </td>
-                        <td> Boxing</td>
+                        <td><span>Boxing</span> </td>
                         <td>9:00 AM - 11:00 AM</td>
                         <td>Aaron Chapman</td>
                         <td>10</td>
@@ -72,8 +70,6 @@ function Integration(){
         </div>
       </div>
     </div>
-  </div>
-</section>
     )
    
 };
